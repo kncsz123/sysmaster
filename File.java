e7d3aaae6a5026fa1455f779c49970a82b4b76be
kncsz123 <3051630882@qq.com>
2024-08-14 13:16:17 +0800

    # 新增type字段，标识是文件还是目录
    (cherry picked from commit 142b45c71ee06445e49f3f810782fb56ecb07332)

--- File.java
+++ File.java
@@ -19,15 +19,18 @@ public class File {
     private String lastmodified;
     @Column
     private long size;
+    @Column
+    private String type; //表示是文件还是目录
 
     public File(){};
 
-    public File(String name, String path, String datestamp, String lastmodified, long size) {
+    public File(String name, String path, String datestamp, String lastmodified, long size, String type) {
         this.name = name;
         this.path = path;
         this.datestamp = datestamp;
         this.lastmodified = lastmodified;
-        this.size = 0;
+        this.size = size;
+        this.type = type;
     }
 
     public String getName() {
@@ -46,14 +49,6 @@ public class File {
         this.path = path;
     }
 
-    public String getDatastamp() {
-        return datestamp;
-    }
-
-    public void setDatastamp(String datastamp) {
-        this.datestamp = datastamp;
-    }
-
     public String getLastmodified() {
         return lastmodified;
     }
@@ -68,5 +63,29 @@ public class File {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String getDatestamp() {
+        return datestamp;
+    }
+
+    public void setDatestamp(String datestamp) {
+        this.datestamp = datestamp;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
